60fed2d070520028f2194d26ac91335fa05cba40
Thibault Allançon <tallancon@gmail.com>
2018-07-26 21:43:01 +0200

    add missing error handling

--- src/cat_file.rs
+++ src/cat_file.rs
@@ -3,35 +3,34 @@ use std::str;
 use object;
 use read_tree;
 
-pub fn cat_file(hash_prefix: &str, mode: &str) {
-    let object = match object::get_object(hash_prefix) {
-        Ok(obj) => obj,
-        Err(why) => {
-            println!("cat-file: error while parsing the object: {:?}", why);
-            return;
-        }
-    };
+#[derive(Debug)]
+pub enum Error {
+    ObjectError(object::Error),
+    TreeError(read_tree::Error),
+    Utf8Error(str::Utf8Error),
+}
+
+pub fn cat_file(hash_prefix: &str, mode: &str) -> Result<(), Error> {
+    let object = object::get_object(hash_prefix).map_err(Error::ObjectError)?;
 
     match mode {
-        "type" => println!("{}", object.obj_type),
-        "size" => println!("{}", object.obj_size),
-        "print" => match object.obj_type.as_str() {
+        "--type" | "-t" | "type" => println!("{}", object.obj_type),
+        "--size" | "-s" | "size" => println!("{}", object.obj_size),
+        "--print" | "-p" | "print" => match object.obj_type.as_str() {
             "blob" | "commit" => {
-                let data = str::from_utf8(&object.data).expect("invalid utf-8 in object data");
+                let data = str::from_utf8(&object.data).map_err(Error::Utf8Error)?;
                 println!("{}", data);
             }
             "tree" => {
-                match read_tree::read_tree(hash_prefix) {
-                    Ok(entries) => {
-                        for entry in entries {
-                            println!("{:o} {} {}", entry.mode, entry.hash, entry.path);
-                        }
-                    }
-                    Err(why) => println!("cat-file: could not read tree object: {:?}", why),
-                };
+                let entries = read_tree::read_tree(hash_prefix).map_err(Error::TreeError)?;
+                for entry in entries {
+                    println!("{:o} {} {}", entry.mode, entry.hash, entry.path);
+                }
             }
             tp => println!("unknown object type: {}", tp),
         },
         fmt => println!("cat-file: unknown option mode: {}", fmt),
     }
+
+    Ok(())
 }
