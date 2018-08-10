3e56e6f3fce7b7dbb26c3ef2ac23ee19233debb1
Thibault Allançon <tallancon@gmail.com>
2018-08-10 07:49:28 +0200

    prevent branching when no commits yet

--- src/builtin/branch.rs
+++ src/builtin/branch.rs
@@ -5,6 +5,18 @@ use std::path::Path;
 use cli;
 use refs;
 
+#[derive(Debug)]
+pub enum Error {
+    HEADNotPointingToCommit,
+    IoError(io::Error),
+}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Error {
+        Error::IoError(e)
+    }
+}
+
 pub fn cmd_branch(args: &[String], flags: &[String]) {
     let accepted_flags = ["--list", "-l"];
     if cli::has_known_flags(flags, &accepted_flags) {
@@ -17,9 +29,8 @@ pub fn cmd_branch(args: &[String], flags: &[String]) {
     }
 }
 
-fn branch(name: &str, flag: &str) -> io::Result<()> {
+fn branch(name: &str, flag: &str) -> Result<(), Error> {
     let cur_branch = refs::read_ref("HEAD")?;
-    let cur_hash = refs::get_ref_hash("HEAD")?;
 
     if flag == "--list" || flag == "-l" || name.is_empty() {
         let refs_dir = Path::new(".git").join("refs").join("heads");
@@ -38,6 +49,11 @@ fn branch(name: &str, flag: &str) -> io::Result<()> {
             }
         }
     } else if !name.is_empty() {
+        let cur_hash = refs::get_ref_hash("HEAD")?;
+        if cur_hash.is_empty() {
+            return Err(Error::HEADNotPointingToCommit);
+        }
+
         refs::write_to_ref(name, &cur_hash)?;
     }
 
